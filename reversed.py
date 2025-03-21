5f3488022c219184575a516460e850c335ddaf2a
mohammedrinshad123 <mohammedrinshad001@gmail.com>
2025-03-21 10:28:54 +0530

    first commit

---
+++ reversed.py
@@ -0,0 +1,4 @@
+my_string = "Hello, World!"
+
+reversed_string = my_string[::-1]
+print("Reversed string:", reversed_string)
---
+++ sortedlist.py
@@ -0,0 +1,4 @@
+my_list = [12, 5, 3, 9, 15]
+
+my_list.sort()
+print("Sorted list:" ,my_list)
